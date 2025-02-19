c95a5fb15cad164e7dfd6654ecc838f05236be60
Didier Grand <didier.grand@gmail.com>
2025-02-19 07:55:38 +0100

    Add success custom message

---
+++ tribe/tickets/blocks/rsvp/messages/success.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Block: RSVP
+ * Messages Sucess
+ *
+ * Override this template in your own theme by creating a file at:
+ * [your-theme]/tribe/tickets/blocks/rsvp/messages/success.php
+ *
+ * See more documentation about our Blocks Editor templating system.
+ *
+ * @link https://evnt.is/1amp Help article for RSVP & Ticket template files.
+ *
+ * @since 4.9.3
+ * @since 4.10.9 Use function for text.
+ *
+ * @version 4.11.3
+ */
+
+?>
+<div class="tribe-block__rsvp__message__success">
+
+	<?php
+	echo "Votre réservation a été enregistrée avec succès !"; ?>
+
+</div>
