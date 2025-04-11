3d89c839bd227d96adfbb7479ff9f95f36a746b6
Didier Grand <didier.grand@gmail.com>
2025-04-11 16:41:45 +0200

    Remove archives

--- functions.php
+++ functions.php
@@ -223,3 +223,21 @@ function custom_image_sizes() {
     );
 }
 add_action( 'after_setup_theme', 'custom_image_sizes' );
+
+add_filter(
+    'get_the_archive_title',
+    function ($title) {
+        if (is_category()) {
+            $title = single_cat_title('', false);
+        } elseif (is_tag()) {
+            $title = single_tag_title('', false);
+        } elseif (is_author()) {
+            $title = get_the_author();
+        } elseif (is_post_type_archive()) {
+            $title = post_type_archive_title('', false);
+        } elseif (is_tax()) {
+            $title = single_term_title('', false);
+        }
+        return $title;
+    }
+);
